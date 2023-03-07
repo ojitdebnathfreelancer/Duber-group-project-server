3a13e2d8a74f62c6e2b08afb423350e8e80b8bac
iftakhar711 <iftakharudiin80@gmail.com>
2023-03-07 21:46:34 +0600

    bookings

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -23,6 +23,7 @@ const Duber = async () => {
     try {
         const TransportsData = client.db('duber').collection('transports');
         const UsersData = client.db('duber').collection('users');
+        const bookingsCollection = client.db('ride').collection('bookings')
 
         app.post('/addtransport', async (req, res) => {
             const transport = req.body
@@ -58,8 +59,53 @@ const Duber = async () => {
             const user = req.body;
             const added = await UsersData.insertOne(user);
             res.send(added);
+            console.log(added);
         });
-        // added user to DB 
+        // added user to DB
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const query = {}
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        });
+        //added ride to db
+
+        app.get('/bookings/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email }
+            const result = await bookingsCollection.find(query).toArray()
+            res.send(result);
+        });
+        //get ride from db
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await bookingsCollection.deleteOne(filter);
+            res.send(result);
+        })
+
+        app.get('/adduser', async (req, res) => {
+            const quary = {}
+            const result = await UsersData.find(quary).toArray();
+            res.send(result);
+        })
+        // get user from DB 
+
+        app.get('/passenger', async (req, res) => {
+            const query = { role: "passenger" }
+            const role = await UsersData.find(query).toArray()
+            res.send(role)
+        })
+        //get passenger from db
+
+        app.get('/company', async (req, res) => {
+            const query = { role: "company" }
+            const role = await UsersData.find(query).toArray()
+            res.send(role)
+        })
+        //get company from db
 
     }
     finally { }
